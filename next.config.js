0f928ce540322efe22e56331ff5bbbd19ca7d096
Your Name <you@example.com>
2022-11-05 19:08:00 -0400

    primeiro commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: {
+    autoPrerender: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
+  autoPrerender: true
 }
 
 module.exports = nextConfig
